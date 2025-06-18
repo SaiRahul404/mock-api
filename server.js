5f9f4022af722b2225487e583d9a41cec98618b8
rahul500325 <rahul.gajawada@wavemaker.com>
2025-06-18 12:39:28 +0530

    Initial commit4: mock API

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const { startEnvironment } = require('@mockoon/cli');
+const { exec } = require('child_process');
 
-startEnvironment({
-  data: require('./mockoon-environment.json'),
-  port: process.env.PORT || 3000
+exec('npx mockoon-cli start --data ./mockoon-environment.json --port 3001 --daemon-off', (err, stdout, stderr) => {
+  if (err) {
+    console.error('Error starting Mockoon:', stderr);
+    process.exit(1);
+  }
+  console.log('Mockoon started:', stdout);
 });
